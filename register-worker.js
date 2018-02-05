6f4d7fcc8418b0fa8ac3ad78e9abe85fc060c39b
Sai Nithesh <sainithesh21@gmail.com>
2018-02-05 17:38:36 +0530

    Delete register-worker.js

--- register-worker.js
+++
@@ -1,8 +0,0 @@
-/**
- * Register the service worker
- */
-if ('serviceWorker' in navigator) {
-	navigator.serviceWorker
-		.register('./firebase-messaging-sw.js')
-		.then(function () { console.log('Service Worker Registered'); });
-}
